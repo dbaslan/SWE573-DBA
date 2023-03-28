055d41cb3d007b8a356425e5e1d7b59e5a807e6e
Deniz Aslan <d_b_aslan@windowslive.com>
2023-03-28 21:31:19 +0300

    Fix secret key leak
    Secret key is no longer stored in settings.py

--- prototype/prototype/settings.py
+++ prototype/prototype/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5k9fr5xzf)felzwy7ozg5eve9twjf46(+b@*8!p89961q!0o-u'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
