4a2a8a518af09e50bd7f6e77bc19ec37713d1151
Deniz Aslan <d_b_aslan@windowslive.com>
2023-03-28 19:08:32 +0300

    Update gitignore

--- MemoShare/users/views.py
+++ MemoShare/users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import User
 
 def users(request):
-    template = loader.get_template("users.html")
-    return HttpResponse(template.render())
+    myusers = User.objects.all().values()
+    template = loader.get_template('all_users.html')
+    context = {
+        'myusers': myusers,
+    }
+    return HttpResponse(template.render(context, request))
