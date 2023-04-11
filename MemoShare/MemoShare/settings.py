8522e9006527416ca8c9efde71ffec1f409334cb
Deniz Aslan <d_b_aslan@windowslive.com>
2023-04-12 00:16:01 +0300

    Update settings.py
    Migrate to PostgreSQL

--- MemoShare/MemoShare/settings.py
+++ MemoShare/MemoShare/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'MemoShare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MemoShareDatabase',
+        'USER': 'mydatabaseuser',
+        "PASSWORD" : str(os.getenv('PASSWORD')),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
