2fe92120a46c0a0762239cf66a73a2dde3bab132
Deniz Aslan <d_b_aslan@windowslive.com>
2023-04-12 12:15:15 +0300

    Update settings.py
    Revert to sqlite3

--- MemoShare/MemoShare/settings.py
+++ MemoShare/MemoShare/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'MemoShare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'MemoShareDatabase',
-        'USER': 'postgres',
-        "PASSWORD" : str(os.getenv('PASSWORD')),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
