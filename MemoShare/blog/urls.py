225d12ec5e87037c9d959f7de989fa5c4fb959a2
Deniz Aslan <d_b_aslan@windowslive.com>
2023-04-12 21:14:01 +0300

    Update urls.py
    Add url of about page

--- MemoShare/blog/urls.py
+++ MemoShare/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('search', views.post_search, name='post_search'),
+    path('about', views.about, name='about'),
 ]
