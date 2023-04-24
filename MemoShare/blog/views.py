bf2119c5fd67930e434320e366958897d458777c
Deniz Aslan <d_b_aslan@windowslive.com>
2023-04-24 13:49:06 +0300

    Update views.py
    change content to text in query

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -55,7 +55,7 @@ def post_search(request):
 
 def post_search(request):
     query = request.GET.get("q", default="")
-    results = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query))
+    results = Post.objects.filter(Q(title__icontains=query) | Q(text__icontains=query))
     context = {'results': results, 'query': query}
     return render(request, 'post_search.html', context)
 
