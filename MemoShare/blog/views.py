0574e2e62b26397deadf90385884a18323cdadb5
Deniz Aslan <d_b_aslan@windowslive.com>
2023-04-24 13:37:36 +0300

    Update views.py
    Add import statement for Q

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
