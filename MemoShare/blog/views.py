a9c7c30fbec30633400edf69967801afe87d85d0
Deniz Aslan <d_b_aslan@windowslive.com>
2023-04-25 20:38:18 +0300

    Update post_list.html and views.py
    Change published_date to posted_date

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -9,7 +9,7 @@ from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(posted_date__lte=timezone.now()).order_by('posted_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -22,7 +22,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.posted_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
