df7ded6600dcfd9b9a643e826d77ba0a2016a870
Deniz Aslan <d_b_aslan@windowslive.com>
2023-04-25 20:31:26 +0300

    Update models.py
    Create class Comment

--- MemoShare/blog/models.py
+++ MemoShare/blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=3000)
-    created_date = models.DateTimeField(default=timezone.now)
+    posted_date = models.DateTimeField(default=timezone.now)
     likes = models.IntegerField(default=0)
     #memory_date = models.DateTimeField(blank=True, null=True)
 
@@ -18,3 +18,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    content = models.TextField(max_length=1000)
+    posted_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_connected = models.ForeignKey(Post, on_delete=models.CASCADE)
