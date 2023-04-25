de5df66622722773cf7d221110ce9f518f463118
Deniz Aslan <d_b_aslan@windowslive.com>
2023-04-25 20:21:31 +0300

    Update views.py
    Expand post_search to include text, author, tags

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -45,8 +45,38 @@ def post_edit(request, pk):
 
 def post_search(request):
     query = request.GET.get("q", default="")
-    results = Post.objects.filter(Q(title__icontains=query) | Q(text__icontains=query))
-    context = {'results': results, 'query': query}
+    text_query = request.GET.get("text", default="")
+    author_query = request.GET.get("author", default="")
+    tag_query = request.GET.get("tag", default="")
+    #date_query = request.GET.get("date", default="")
+    #location_query = request.GET.get("location", default="")
+
+    results = Post.objects.all()
+
+    if query:
+        results = results.filter(
+            Q(title__icontains=query) |
+            Q(text__icontains=query) |
+            Q(author__icontains=query) |
+            Q(tag__name=query)
+            #Q(date__icontains=query) |
+            #Q(location__icontains=query) |
+        )
+    
+    if text_query:
+        results = results.filter(text__icontains=text_query)
+    if tag_query:
+        results = results.filter(date__icontains=tag_query)
+    if author_query:
+        results = results.filter(author__icontains=author_query)
+
+    context = {
+        'results': results,
+        'query': query,
+        'text_query': text_query,
+        'tag_query': tag_query,
+        'author_query': author_query,
+    }
     return render(request, 'blog/post_search.html', context)
 
 def about(request):
