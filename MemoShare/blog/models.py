5cf701af00a5c0b8db531df04db95b6886264f60
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-09 02:23:45 +0300

    Update models.py
    Import post_save

--- MemoShare/blog/models.py
+++ MemoShare/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Tag(models.Model):
     name = models.CharField(max_length=32)
