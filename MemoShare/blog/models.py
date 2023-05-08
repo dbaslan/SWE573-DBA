734798a995b7d5e934aae3a90476526c4540d7d3
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-09 02:25:43 +0300

    Update models.py
    Add save_profile() and create_profile()

--- MemoShare/blog/models.py
+++ MemoShare/blog/models.py
@@ -38,3 +38,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
