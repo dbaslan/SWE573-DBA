9c5b674db0b52fe79399ed5e4ba786ddca52aa7a
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-08 23:29:44 +0300

    Update views.py
    Import user creation and logging forms

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from django.utils import timezone
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 from .models import Post
 from .forms import PostForm
 
