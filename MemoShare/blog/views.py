38221b69f091275e9043fc015345fa027b0bae52
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-08 21:34:37 +0300

    Update views.py
    Remove text_query (combined with query)

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -45,7 +45,6 @@ def post_edit(request, pk):
 
 def post_search(request):
     query = request.GET.get("q", default="")
-    text_query = request.GET.get("text", default="")
     author_query = request.GET.get("author", default="")
     tag_query = request.GET.get("tag", default="")
     #date_query = request.GET.get("date", default="")
@@ -54,9 +53,7 @@ def post_search(request):
     results = Post.objects.all()
 
     if query:
-        results = results.filter(title__icontains=text_query)
-    if text_query:
-        results = results.filter(text__icontains=text_query)
+        results = results.filter(Q(title__icontains=query) | Q(text__icontains=query))
     if tag_query:
         results = results.filter(tags__name__icontains=tag_query)
     if author_query:
@@ -65,7 +62,6 @@ def post_search(request):
     context = {
         'results': results,
         'query': query,
-        'text_query': text_query,
         'tag_query': tag_query,
         'author_query': author_query,
     }
