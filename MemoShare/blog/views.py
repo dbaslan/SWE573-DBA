9094650d0f51cd83cda012926315a8d794c4d8d3
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-09 02:08:21 +0300

    Update views.py
    Rewrite user_login()

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -98,15 +98,22 @@ def user_register(request):
     return render(request, 'blog/user_register.html', {'form': form})
 
 def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('profile')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'blog/login.html', {'form': form})
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You have logged in as {username}.")
+				return redirect("profile")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="blog/user_login.html", context={"login_form":form})
 
 def user_logout(request):
     logout(request)
