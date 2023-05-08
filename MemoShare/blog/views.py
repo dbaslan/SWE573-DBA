a31551bf2306fa274adc6d36b99bb067c6427eb6
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-08 23:28:53 +0300

    Update views.py
    Optimize imports

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from django.utils import timezone
 from .models import Post
