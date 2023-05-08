af49742c74d06308c297294b02fe843a2da36cf5
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-08 23:38:29 +0300

    Update views.py
    Add user_login()

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -86,3 +86,14 @@ def user_register(request):
     else:
         form = UserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
