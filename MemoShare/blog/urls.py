d35e8552d56289fff6a9704b203ffd5620fde864
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-09 01:56:36 +0300

    Update urls.py
    Fix user_logout

--- MemoShare/blog/urls.py
+++ MemoShare/blog/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('profile', views.user_profile, name='user_profile'),
     path('register', views.user_register, name='user_register'),
     path('login', views.user_login, name='user_login'),
-    path('logout', views.user_logout, name='post_list')
+    path('logout', views.user_logout, name='user_logout')
 ]
