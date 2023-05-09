71f903672fe495401a9cdf1cd7a0f4213dfad464
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-09 20:14:31 +0300

    Update urls.py
    Add post_random URL

--- MemoShare/blog/urls.py
+++ MemoShare/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_random, name='post_random'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('search', views.post_search, name='post_search'),
@@ -12,5 +13,5 @@ urlpatterns = [
     path('register', views.user_register, name='user_register'),
     path('login', views.user_login, name='user_login'),
     path('logout', views.user_logout, name='user_logout'),
-    path("profile", views.user_profile, name = "user_profile")
+    path("profile", views.user_profile, name="user_profile")
 ]
