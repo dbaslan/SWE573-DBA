266d39ad17aa4c748d5b454ed721c8eb56edab37
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-09 12:39:38 +0300

    Update urls.py
    Add profile URL

--- MemoShare/blog/urls.py
+++ MemoShare/blog/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('profile', views.user_profile, name='user_profile'),
     path('register', views.user_register, name='user_register'),
     path('login', views.user_login, name='user_login'),
-    path('logout', views.user_logout, name='user_logout')
+    path('logout', views.user_logout, name='user_logout'),
+    path("profile", views.user_profile, name = "user_profile")
 ]
