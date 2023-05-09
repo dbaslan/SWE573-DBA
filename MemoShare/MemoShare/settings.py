90c3c9041dfb74e412617719f3ba15305b9a57bf
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-09 12:23:58 +0300

    Update settings.py
    Add installed apps

--- MemoShare/MemoShare/settings.py
+++ MemoShare/MemoShare/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "users"
+    "users",
+    'django.contrib.sites',
+    "MemoShare",
+    "blog"
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
