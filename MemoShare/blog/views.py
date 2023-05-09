f39080b7f9ff14719a3876546fe826a534c57346
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-09 12:42:51 +0300

    Update views.py
    add blog/ to path

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -125,4 +125,4 @@ def user_profile(request):
     profile = Profile.objects.get(user=user)
     posts = Post.objects.filter(author=user).order_by('posted_date')
     context = {'user': user, 'profile': profile, 'posts': posts}
-    return render(request, 'user_profile.html', context)
+    return render(request, 'blog/user_profile.html', context)
