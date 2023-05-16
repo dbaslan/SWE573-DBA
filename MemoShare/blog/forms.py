7d4089396b146e9892a213d9e5ca985206e3510c
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-16 17:01:07 +0300

    Update forms.py
    Add memory date to PostForm

--- MemoShare/blog/forms.py
+++ MemoShare/blog/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import Post
 
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ("title", "text", "tags")
+        fields = ("title", "text", "tags", "memory_date")
+        widgets = {
+            'memory_date': DateInput(),
+        }
