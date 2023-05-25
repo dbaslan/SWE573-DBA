f8b98816ee56cf8c395092b6e85b2230782db25b
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-25 19:55:11 +0300

    Update views.py
    Import auth_views

--- MemoShare/blog/views.py
+++ MemoShare/blog/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import Post, Profile, Like, Comment
