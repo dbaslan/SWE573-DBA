8159e9673b0b826705406e13f2a9f5e8d63c064e
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-25 19:42:28 +0300

    Update urls.py
    Add user_mail_edit url

--- MemoShare/blog/urls.py
+++ MemoShare/blog/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('user/<str:usernamex>/', views.user_page, name='user_page'),
     path('profile', views.user_profile, name='user_profile'),
     path('profile/edit', views.user_profile_edit, name='user_profile_edit'),
+    path('profile/mail', views.user_mail_edit, name='user_mail_edit'),
     path('register', views.user_register, name='user_register'),
     path('login', views.user_login, name='user_login'),
     path('logout', views.user_logout, name='user_logout'),
