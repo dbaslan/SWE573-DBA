8c770d1378591b71be96f8e65dc90ca05df8fafe
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-25 19:57:22 +0300

    Update urls.py
    Add PasswordChangeView url

--- MemoShare/blog/urls.py
+++ MemoShare/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -14,6 +15,7 @@ urlpatterns = [
     path('profile', views.user_profile, name='user_profile'),
     path('profile/edit', views.user_profile_edit, name='user_profile_edit'),
     path('profile/mail', views.user_mail_edit, name='user_mail_edit'),
+    path('profile/password', auth_views.PasswordChangeView.as_view()),
     path('register', views.user_register, name='user_register'),
     path('login', views.user_login, name='user_login'),
     path('logout', views.user_logout, name='user_logout'),
