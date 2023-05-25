c303b4d9b2112d293dc46db5277fac98547d68bf
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-25 15:38:14 +0300

    Update admin.py
    Integrate django_google_maps

--- MemoShare/blog/admin.py
+++ MemoShare/blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post, Profile
+from django_google_maps import widgets as map_widgets
+from django_google_maps import fields as map_fields
 
 admin.site.register(Post)
 admin.site.register(Profile)
+
+class PostAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        map_fields.AddressField: {'widget': map_widgets.GoogleMapsAddressWidget},
+    }
