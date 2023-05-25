52b29d49f13a019d9a22af5a817ccfb5d8f66578
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-25 18:17:56 +0300

    Update models.py
    Add null and blunk parameter to Profile.name

--- MemoShare/blog/models.py
+++ MemoShare/blog/models.py
@@ -53,7 +53,7 @@ class Comment(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=155)
+    name = models.CharField(max_length=155, null=True, blank=True)
     bio = models.TextField(max_length=1000, blank=True)
     location = map_fields.AddressField(max_length=200, blank=True, null=True)
     birthdate = models.DateField(null=True, blank=True)
