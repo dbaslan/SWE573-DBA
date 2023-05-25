d2eaaa7f3278cf8fb5f55bffcc8d2457f8abf17f
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-25 16:53:07 +0300

    Update models.py
    Adust attributes for Profile()

--- MemoShare/blog/models.py
+++ MemoShare/blog/models.py
@@ -53,8 +53,8 @@ class Comment(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(max_length=1000, blank=True)
+    location = map_fields.AddressField(max_length=200, blank=True, null=True)
     birthdate = models.DateField(null=True, blank=True)
 
     def __str__(self):
