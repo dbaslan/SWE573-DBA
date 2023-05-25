7c63d7a7dea613147c11e03db75d9689f99d5c7b
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-25 17:52:02 +0300

    Update forms.py
    Add ProfileForm()

--- MemoShare/blog/forms.py
+++ MemoShare/blog/forms.py
@@ -1,15 +1,19 @@
 from django import forms
-from .models import Post
+from .models import Post, Profile
 
 
 class DateInput(forms.DateInput):
     input_type = 'date'
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
         fields = ("title", "text", "tags", "location", "memory_date")
         widgets = {
             'memory_date': DateInput(),
         }
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["first_name", "Last_name", "birthdate", "location", "bio"]
