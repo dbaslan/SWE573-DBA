4e5ea72c2bbdc40b31445ed652fc52d7ffd99896
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-26 19:47:22 +0300

    Update tests.py
    Remove previous tests and add model tests

--- MemoShare/blog/tests.py
+++ MemoShare/blog/tests.py
@@ -1,68 +1,114 @@
-from django.test import TestCase, Client
+from django.utils import timezone
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Profile
 from django.urls import reverse
-from .models import Post, User
 
-class LoginTest(TestCase):
+# Test info page
+class AboutTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about")
+        self.assertEqual(response.status_code, 200)
 
-    # Create test user
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(username='test', password='motdepasse')
-        self.user.save()
+    def test_url_available_by_name(self):  
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
 
-    def tearDown(self):
-        self.user.delete()
+    def test_template_name_correct(self):  
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "blog/about.html")
 
-    def test_authentication(self):
-        user = authenticate(username='test', password='motdepasse')
-        self.assertTrue((user is not None) and user.is_authenticated)
+# Test home page
+class HomeTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
 
-    def test_username(self):
-        user = authenticate(username='blablalba', password='motdepasse')
-        self.assertFalse(user is not None and user.is_authenticated)
+    def test_url_available_by_name(self):  
+        response = self.client.get(reverse("post_list"))
+        self.assertEqual(response.status_code, 200)
 
-    def test_pssword(self):
-        user = authenticate(username='test', password='blablabla')
-        self.assertFalse(user is not None and user.is_authenticated)
+    def test_template_name_correct(self):  
+        response = self.client.get(reverse("post_list"))
+        self.assertTemplateUsed(response, "blog/post_list.html")
 
-class UnitTest(TestCase):
-    
+# Test Post() model
+class PostTests(TestCase):
+
     def setUp(self):
 
         # Create test user
-        self.user = get_user_model().objects.create_user(username='testuser', password='testpassword')
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
 
-        # Create test posts
-        self.post1 = Post.objects.create(title='Test Post', text='This is a test.', author=self.user)
-        self.post2 = Post.objects.create(title='Another Test Post 2', text='This is another test.', author=self.user)
-        self.post3 = Post.objects.create(title='Yet Another Test Post', text='This is yet another test.', author=self.user)
+        # Create test post
+        self.post = Post.objects.create(
+            author=self.user,
+            title='Test Post',
+            text='This is the test post text.',
+            posted_date=timezone.now(),
+            memory_date=timezone.now(),
+            location='Test Location',
+            image='images/test.png'
+        )
 
-    # Test user_login() and user_logout()
-    def test_user_login_view(self):
-        response = self.client.get(reverse('user_login'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.post1.title)
-        self.assertContains(response, self.post2.title)
-        self.assertContains(response, self.post3.title)
-        response = self.client.get(reverse('user_logout'))
+    # Test post creation
+    def test_post_creation(self):
+        self.assertEqual(self.post.author, self.user)
+        self.assertEqual(self.post.title, 'Test Post')
+        self.assertEqual(self.post.text, 'This is the test post text.')
+        self.assertIsNotNone(self.post.posted_date)
+        self.assertIsNotNone(self.post.memory_date)
+        self.assertEqual(self.post.location, 'Test Location')
+        self.assertEqual(self.post.image, 'images/test.png')
 
- 	# Test post_list()
-    def test_post_list_view(self):
-        response = self.client.get(reverse('post_list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.post1.title)
-        self.assertContains(response, self.post2.title)
-        self.assertContains(response, self.post3.title)
+    # Test __str__() method
+    def test_post_string_representation(self):
+        expected_str = self.post.title
+        self.assertEqual(str(self.post), expected_str)
 
-    # Test post_detail()
-    def test_post_detail_view(self):
-        response = self.client.get(reverse('post_detail', kwargs={'pk': self.post1.pk}))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.post1.title)
-        self.assertContains(response, self.post1.text)
+# Test Profile() model
+class ProfileTest(TestCase):
 
-    # Test post_new()
-    def test_post_new_view(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.post(reverse('post_new'), {'title': 'New Post', 'text': 'Hello World.'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(Post.objects.count(), 3)
+    def setUp(self):
+
+        # Create a test user
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+
+        # Create a profile for the test user
+        self.profile = Profile.objects.create(
+            user=self.user,
+            name='Test User',
+            bio='This is the test user bio',
+            location='Test Location',
+            birthdate='2000-01-01',
+            avatar='profile_pics/test.png'
+        )
+
+    # Test profile creation
+    def test_profile_creation(self):
+        self.assertEqual(self.profile.user, self.user)
+        self.assertEqual(self.profile.name, 'Test User')
+        self.assertEqual(self.profile.bio, 'This is the test user bio')
+        self.assertEqual(self.profile.location, 'Test Location')
+        self.assertEqual(self.profile.birthdate, '2000-01-01')
+        self.assertEqual(self.profile.avatar, 'profile_pics/test.png')
+
+    # Test __str__() method 
+    def test_profile_str(self):
+        expected_str = self.user.username
+        self.assertEqual(str(self.profile), expected_str)
+
+    # Test automatic creation of profile on User creation
+    def test_profile_auto_creation(self):
+        new_user = User.objects.create_user(
+            username='newuser',
+            password='newpassword'
+        )
+        profile = Profile.objects.get(user=new_user)
+        self.assertIsNotNone(profile)
