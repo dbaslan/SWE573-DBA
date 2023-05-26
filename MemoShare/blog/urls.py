b81a84fecab57fff488de7a10c523f0ee5621e3e
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-26 05:02:11 +0300

    Update urls.py
    Add static urls for media

--- MemoShare/blog/urls.py
+++ MemoShare/blog/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('register', views.user_register, name='user_register'),
     path('login', views.user_login, name='user_login'),
     path('logout', views.user_logout, name='user_logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
