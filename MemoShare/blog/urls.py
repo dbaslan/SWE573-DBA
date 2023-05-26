af68d09312f2dd895f0596bf187adc376b7fbfbb
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-26 05:00:12 +0300

    Update urls.py
    Import static and settings

--- MemoShare/blog/urls.py
+++ MemoShare/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
