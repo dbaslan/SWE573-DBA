f4db39ff4fd46a4bed57f849990a05b8b6558bf1
Deniz Aslan <d_b_aslan@windowslive.com>
2023-05-27 13:22:02 +0300

    Update settings.py
    Change ALLOWED_HOSTS to "*"

--- MemoShare/MemoShare/settings.py
+++ MemoShare/MemoShare/settings.py
@@ -29,7 +29,7 @@ EASY_MAPS_GOOGLE_KEY = creds.EASY_MAPS_GOOGLE_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
